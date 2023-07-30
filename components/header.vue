1200aaebed603d166397b0fb2362f23f8ccee1de
Muhammad Akmal Beliuk <akmalbeliuk@gmail.com>
2023-07-30 21:18:56 +0700

    First commit for this project

---
+++ components/header.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="w-full h-[50px] bg-[#F44F1A] border-black border-b-[1px]">
+    <div class="flex text-base justify-between max-w-screen-lg mx-auto pt-2">
+      <div class="py-1 px-2 rounded-[30px] border border-black">Portofolio</div>
+      <div class="py-1 px-2 rounded-[30px] border border-black">2023</div>
+    </div>
+  </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ pages/portofolio.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <div class="w-full bg-[#F44F1A]">
+
+    </div>
+  </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
